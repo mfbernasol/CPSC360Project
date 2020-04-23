8b932018e2f5f8f9bc0a32f18646ccee2471732c
mfbernasol <mfbernasol@gmail.com>
2020-04-23 01:51:12 -0500

    changing functions for drop down menu to retreive data. Work in progress

--- client_code.js
+++ client_code.js
@@ -1,35 +1,40 @@
 //--THIS PART OF CODE KEEPS JAVASCRIPT VARIABLE UPDATED TO FORM INPUT--//
 //select text input in form and store to a variable
-const textInput = document.getElementById('mag');
-let mag = textInput.value;
-const textInput = document.getElementById('place');
-let place = textInput.value;
-const textInput = document.getElementById('date');
-let date = textInput.value;
-const textInput = document.getElementById('month');
-let month = textInput.value;
-const textInput = document.getElementById('year');
-let year = textInput.value;
+const howManyInput = document.getElementById('mag');
+let howMany = howManyInput.value;
+const magInput = document.getElementById('place');
+let place = magInput.value;
+const dateInput = document.getElementById('date');
+let date = dateInput.value;
+const monthInput = document.getElementById('month');
+let month = monthInput.value;
+const yearInput = document.getElementById('year');
+let year = yearInput.value;
 
 
 
 //drop down menu for WHAT
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+//RETREIVING VALUES
+
+//var e = document.getElementById("country");
+var result = e.options[e.selectedIndex].text;
 
 //drop down menu for TIMEFRAME
 function myFunction2() {
@@ -50,24 +55,27 @@ function myFunction2() {
     }
   }
 
-  //drop down menu for COUNTRY 
-  function myFunction3() {
-    document.getElementById("myDropdown3").classList.toggle("show");
-  }
+  //Retrieve from drop down menu for COUNTRY 
+var e = document.getElementById("country");
+var result = e.options[e.selectedIndex].text;
+
+//   function myFunction3() {
+//     document.getElementById("myDropdown3").classList.toggle("show");
+//   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
   
   
   
@@ -83,14 +91,14 @@ function myFunction2() {
        //make request to API using axios
        //axios lets us easily get API request
        //make sure to include axios CDN in html
-       axios.get("http://localhost:8000/employees/?lastname="+lastname).then(function(resp){
+       axios.get("http://localhost:8000/earthquakes/?howmany="+howmany.then(function(resp){
                                          outputdata(resp);})
                                          .catch(function(error){
                                            console.log(error);
                                          })
        
        
-       textInput.value="";//clear form input box
+       howManyInput.value="";//clear form input box
        }	
  )
 
