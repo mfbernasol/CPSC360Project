488e97e54067a90b0db5e19a905335a1fa5f44bb
mfbernasol <46685981+mfbernasol@users.noreply.github.com>
2020-04-24 16:11:52 -0500

    updated client

--- client_code.js
+++ client_code.js
@@ -1,59 +1,89 @@
 //--THIS PART OF CODE KEEPS JAVASCRIPT VARIABLE UPDATED TO FORM INPUT--//
 //select text input in form and store to a variable
-const howManyInput = document.getElementById('howmany');
-let howMany = howManyInput.value;
+const howManyInput = document.getElementById('howmany'); 
+let howmany = howManyInput.value;
 
-const magInput = document.getElementById('mag');
-let magnitude = magInput.value;
+const whatInput = document.getElementById('what')
+let what = whatInput.value
+
+const minInput = document.getElementById('minmag');
+let minMag= minInput.value;
+
+const maxInput = document.getElementById('maxmag')
+let maxMag = maxInput.value
 
 const dateInput = document.getElementById('date');
-let date = dateInput.value;
+let date = dateInput.value
 
-const monthInput = document.getElementById('month');
-let month = monthInput.value;
 
-const yearInput = document.getElementById('year');
-let year = yearInput.value;
+const placeInput = document.getElementById('country')
+let country = placeInput.value
+
+
 
 
 //RETREIVING VALUES 
 
 //retrieves input from How Many field
 howManyInput.addEventListener('input',function(e){
-  howMany.e.target.value
-  }
-)
-
-//retreives input from WHAT 
-  var w = document.getElementById("what")
-  var result = w.options[selectedIndex].text;
+  console.log(howManyInput.value)
+  howmany = e.target.value
+})
 
 
-
-//retrieves input from TIME FRAME
-var t = document.getElementById("timeframe")
-var timeResult = t.options[selectedIndex].text
-
-
-//retreives input from COUNTRY
-var c = document.getElementById('country')
-var countryResult = c.options[selectedIndex].text
+//retreives input from WHAT
+whatInput.addEventListener('input',function(e){
+    console.log(whatInput.value)
+    what = e.target.value
+  })
 
 
+//retreives from min mag
+minInput.addEventListener('input',function(e){
+  console.log(minInput.value)
+  minMag = e.target.value
+})
+
+//retrieves from max mag
+maxInput.addEventListener('input',function(e){
+  console.log(maxInput.value)
+  maxMag = e.target.value
+})
+
+//retrives from max
+dateInput.addEventListener('input',function(e){
+  console.log(dateInput)
+  date = e.target.value
+})
+
+placeInput.addEventListener('input',function(e){
+  console.log(placeInput)
+  country = placeInput.value
+
+})
+
+
+
+//Retrieves input when form is submitted 
 const form = document.querySelector('form')
-
+console.log(form)
 form.addEventListener('submit',function(e){
   e.preventDefault();
 
-  axios.get("http://localhost:8000/earthquakes/?howmany="+howmany +"&mag" + magnitude + "&month" + month + "&country" + countryResult).then(function(resp){outputdata(resp);}).catch(function(error){console.log(error);})
-          
+  axios.get("http://localhost:8000/earthquakes/?howmany="+howmany+"&maxMag="+maxMag+"&what="+what+"&minMag="+minMag+"&timeframe="+date+"&place="+place.toLowerCase()).then(function(resp){
+    outputdata(resp);})
+    .catch(function(error){
+      console.log(error);
+    })          
   
   
   howManyInput.value= ""
-  }
-
-
-  )
+  whatInput.value=""
+  minInput.value=""
+  maxInput.value=""  
+  dateInput.value=""
+  placeInput.value=""
+})
 
 
 function outputData(resp){
@@ -61,22 +91,34 @@ function outputData(resp){
   console.log(resp.data.length)
 
 
-  let outputheading = document.querySelector('.outputheading')
-  let earthquakedata = document.querySelector('.earthquakedata')
+  let earthquakeCountry = document.querySelector('.earthquakeCountry')
+  let earthquakeMag = document.querySelector('.earthquakeMag')
+  let earthquakeDay = document.querySelector('.earthquakeDay')
+  let earthquakeMonth = document.querySelector('.earthquakeMonth')
+  let earthquakeYear = document.querySelector('earthquakeYear')
   let errormsg = document.querySelector('.error-message')
 
   let earthquakelist=[]
   let earthquakestr=''
   
-  if(resp.data.length){
-    for(i=0;i<res.data.length;i++){
-      earthquakestr="A magnitude" + resp.data[i].mag + "earthquake occurred" + resp.data[i].place + " of"+ resp.data[i].month
-    }
-  }else{
-    errormsg.innerHTML = "No earthquake data found"
-  }
+  if (resp.data.length) { 
+		for (i = 0; i < resp.data.length; i++) {
+			earthquakeStr= "A magnitude " + resp.data[i].maxMag + " earthquake occurred " + resp.data[i].place + " on " + resp.data[i].date + ".";
+			earthquakeList.push(earthquakestr);	
+			}
+
+	 
+
+	} else { 
+		errormsg.innerHTML = " No earthquakes found within those data inputs."
+		console.log(errormsg.innerHTML);
+	}	
   
 
 }
+
+
+
+
 
 
